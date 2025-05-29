88872fb52ca0233d5de9eda6aaf7101fdae812f2
Uliana <ulianav95v@gmail.com>
2025-05-28 18:22:42 -0600

    style: style header and footer

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Switch } from '@headlessui/react';
+
+const Header = () => {
+  return (
+    <header className="h-20 w-full flex justify-between items-center px-4 bg-foreground shadow-sm">
+      <h1 className="text-background text-3xl font-bold">Storyboard Editor</h1>
+      <div className="flex items-center gap-4">
+        <button className="rounded-xl py-2 px-4 bg-pantone text-white">
+          Export as PDF
+        </button>
+        <Switch
+          //   checked={enabled}
+          //   onChange={toggleTheme}
+          className="group relative flex h-7 w-14 cursor-pointer rounded-xl bg-foreground p-1 ease-in-out focus:not-data-focus:outline-none data-checked:bg-background data-focus:outline data-focus:outline-white"
+        >
+          <span
+            aria-hidden="true"
+            className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-cerulean shadow-lg ring-0 transition duration-200 ease-in-out group-data-checked:translate-x-7"
+          />
+        </Switch>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
