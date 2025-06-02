1686161a416ab99d9cf3d6e75a79bb2351609a03
Uliana <ulianav95v@gmail.com>
2025-06-02 17:27:20 -0600

    feat: add storyboard service and input types

---
+++ server/src/modules/storyboard/storyboard.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../prisma/prisma.service';
+import { CreateStoryboardInput } from './dto/create-storyboard.input';
+import { UpdateStoryboardInput } from './dto/update-storyboard.input';
+
+@Injectable()
+export class StoryboardService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.storyboard.findMany({
+      include: {
+        scenes: true,
+      },
+    });
+  }
+
+  findOne(id: string) {
+    return this.prisma.storyboard.findUnique({
+      where: { id },
+      include: {
+        scenes: true,
+      },
+    });
+  }
+
+  create(data: CreateStoryboardInput) {
+    return this.prisma.storyboard.create({
+      data,
+    });
+  }
+
+  update(id: string, data: UpdateStoryboardInput) {
+    return this.prisma.storyboard.update({
+      where: { id },
+      data,
+    });
+  }
+
+  delete(id: string) {
+    return this.prisma.storyboard.delete({
+      where: { id },
+    });
+  }
+}
