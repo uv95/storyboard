e74c82f6289e4f581a484719f60d81bd2d069bf5
Uliana <ulianav95v@gmail.com>
2025-06-20 11:00:39 -0600

    feat: add queries and mutations

---
+++ client/src/features/scene/graphql.ts
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+const FIELDS = gql`
+  fragment SceneFields on Scene {
+    id
+    icon
+    description
+    order
+    title
+  }
+`;
+
+export const GET_STORYBOARD_SCENES = gql`
+  ${FIELDS}
+  query getStoryboardScenes($storyboardId: UUID!) {
+    ...SceneFields
+  }
+`;
+
+export const GET_SCENE = gql`
+  ${FIELDS}
+  query getScene($id: UUID!) {
+    ...SceneFields
+  }
+`;
+
+export const CREATE_SCENE = gql`
+  ${FIELDS}
+  mutation createScene($data: CreateSceneInput!) {
+    createScene(data: $data) {
+      ...SceneFields
+    }
+  }
+`;
+
+export const UPDATE_SCENE = gql`
+  ${FIELDS}
+  mutation updateScene($data: UpdateSceneInput!, $id: UUID!) {
+    updateScene(data: $data, id: $id) {
+      ...SceneFields
+    }
+  }
+`;
+
+export const DELETE_SCENE = gql`
+  mutation deleteScene($id: UUID!) {
+    deleteScene(id: $id) {
+      id
+    }
+  }
+`;
