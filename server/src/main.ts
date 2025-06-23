46f3922dfed98d8c4eb6bc99394d8a684d8557e8
Uliana <ulianav95v@gmail.com>
2025-06-23 14:20:36 -0600

    fix: cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:4000'],
     credentials: true,
   });
   await app.listen(PORT || 3001, () =>
